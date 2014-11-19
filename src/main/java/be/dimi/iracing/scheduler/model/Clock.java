6f04d7cbb25954d4b1be9407d079030f7606498f
dimitri <dimi.s89@hotmail.com>
2014-11-19 11:43:46 +0100

    changed menu opening file. Added serialization to model added gmt and local clock

--- src/main/java/be/dimi/iracing/scheduler/model/Clock.java
+++ src/main/java/be/dimi/iracing/scheduler/model/Clock.java
@@ -12,7 +12,6 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
-import java.util.Locale;
 import java.util.TimeZone;
 
 /**
